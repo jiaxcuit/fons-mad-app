03498070659e72ce1072d5146f0f95a032db5736
jiaxcuit <2454550030@qq.com>
2022-07-10 00:34:24 +0100

    attempted to export ui design from figma

---
+++ community_test/lib/format.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+
+class StartingpageforeveryoneWidget extends StatefulWidget {
+  @override
+  _StartingpageforeveryoneWidgetState createState() =>
+      _StartingpageforeveryoneWidgetState();
+}
+
+class _StartingpageforeveryoneWidgetState
+    extends State<StartingpageforeveryoneWidget> {
+  @override
+  Widget build(BuildContext context) {
+// Figma Flutter Generator StartingpageforeveryoneWidget - COMPONENT
+    return Container(
+        width: 360,
+        height: 800,
+        child: Stack(children: <Widget>[
+          Positioned(
+              top: 0,
+              left: -33,
+              child: Container(
+                  width: 360,
+                  height: 800,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    color: Color.fromRGBO(183, 255, 220, 1),
+                  ),
+                  child: Stack(children: <Widget>[]))),
+          Positioned(
+              top: 701,
+              left: 27,
+              child: Text(
+                'MY COMMUNITY/ MY PROFILE',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Farro',
+                    fontSize: 20,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 13,
+              left: 14,
+              child: Container(
+                  width: 237,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    border: Border.all(
+                      color: Color.fromRGBO(0, 0, 0, 1),
+                      width: 1,
+                    ),
+                  ))),
+          Positioned(
+              top: 15,
+              left: 214,
+              child: Container(
+                  width: 32,
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                  child: Stack(children: <Widget>[
+                    Positioned(
+                      top: 2,
+                      left: 2,
+                      child: Image.asset('assets/vector.png'),
+                    ),
+                  ]))),
+          Positioned(
+              top: 12,
+              left: 258,
+              child: Container(
+                  width: 41,
+                  height: 43.10256576538086,
+                  child: Stack(children: <Widget>[
+                    Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                            width: 41,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(2),
+                                topRight: Radius.circular(2),
+                                bottomLeft: Radius.circular(2),
+                                bottomRight: Radius.circular(2),
+                              ),
+                              color: Color.fromRGBO(204, 255, 139, 1),
+                            ))),
+                    Positioned(
+                        top: 9,
+                        left: 7,
+                        child: Text(
+                          'SIGN\ IN',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color.fromRGBO(0, 0, 0, 1),
+                              fontFamily: 'Farro',
+                              fontSize: 10,
+                              letterSpacing:
+                                  0 /*percentages not used in flutter. defaulting to zero*/,
+                              fontWeight: FontWeight.normal,
+                              height: 1),
+                        )),
+                  ]))),
+          Positioned(
+              top: 20,
+              left: 17,
+              child: Text(
+                'Search for keywords---\ Food/Water... for existing cases',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 0.4699999988079071),
+                    fontFamily: 'Farro',
+                    fontSize: 10,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 9,
+              left: 306,
+              child: Container(
+                  width: 39,
+                  height: 39,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                  child: Stack(children: <Widget>[
+                    Positioned(
+                      top: -7.152557373046875e-7,
+                      left: -7.152557373046875e-7,
+                      child: Image.asset('assets/vectorsmile.png'),
+                    ),
+                  ]))),
+          Positioned(
+            top: 61,
+            left: 14,
+            child: Image.asset('assets/rectangle41.png'),
+          ),
+          Positioned(
+              top: 424,
+              left: 17,
+              child: Container(
+                  width: 324,
+                  height: 168,
+                  child: Stack(children: <Widget>[
+                    Positioned(
+                        top: 0,
+                        left: 172,
+                        child: Container(
+                            width: 152,
+                            height: 168,
+                            child: Stack(children: <Widget>[
+                              Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: Container(
+                                      width: 152,
+                                      height: 168,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(12),
+                                          topRight: Radius.circular(12),
+                                          bottomLeft: Radius.circular(12),
+                                          bottomRight: Radius.circular(12),
+                                        ),
+                                        color: Color.fromRGBO(255, 255, 255, 1),
+                                        border: Border.all(
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                          width: 1,
+                                        ),
+                                      ))),
+                              Positioned(
+                                  top: 15,
+                                  left: 25.16556167602539,
+                                  child: Text(
+                                    'Offer\ Help',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(0, 0, 0, 1),
+                                        fontFamily: 'Farro',
+                                        fontSize: 20,
+                                        letterSpacing:
+                                            0 /*percentages not used in flutter. defaulting to zero*/,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1),
+                                  )),
+                            ]))),
+                    Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                            width: 151,
+                            height: 168,
+                            child: Stack(children: <Widget>[
+                              Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: Container(
+                                      width: 151,
+                                      height: 168,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(12),
+                                          topRight: Radius.circular(12),
+                                          bottomLeft: Radius.circular(12),
+                                          bottomRight: Radius.circular(12),
+                                        ),
+                                        color: Color.fromRGBO(255, 255, 255, 1),
+                                        border: Border.all(
+                                          color: Color.fromRGBO(0, 0, 0, 1),
+                                          width: 1,
+                                        ),
+                                      ))),
+                              Positioned(
+                                  top: 15,
+                                  left: 25,
+                                  child: Text(
+                                    'Request\ Help',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(0, 0, 0, 1),
+                                        fontFamily: 'Farro',
+                                        fontSize: 20,
+                                        letterSpacing:
+                                            0 /*percentages not used in flutter. defaulting to zero*/,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1),
+                                  )),
+                            ]))),
+                  ]))),
+          Positioned(
+              top: 610,
+              left: 17,
+              child: Container(
+                  width: 324,
+                  height: 66,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.25),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    border: Border.all(
+                      color: Color.fromRGBO(0, 0, 0, 1),
+                      width: 1,
+                    ),
+                  ))),
+          Positioned(
+              top: 623,
+              left: 44,
+              child: Text(
+                'Find Support Nearby',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Farro',
+                    fontSize: 20,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 69,
+              left: 26,
+              child: Text(
+                'Scroll up for more...',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Farro',
+                    fontSize: 15,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 91,
+              left: 14,
+              child: Divider(color: Color.fromRGBO(0, 0, 0, 1), thickness: 1)),
+        ]));
+  }
+}
---
+++ community_test/lib/main.dart
@@ -0,0 +1,42 @@
+/*
+with this code, we are able to get designs from figma to the emulator
+however the positions of widgets seem to be hard-coded/fixed, so they cannot adapt to a different screen dimension automatically.
+*/
+
+import 'package:flutter/material.dart';
+
+import 'format.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: StartingpageforeveryoneWidget(),
+    );
+  }
+}
